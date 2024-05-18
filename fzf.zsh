ae3a03bda866afd29f2197d4054044ad3f61862a
thederpykrafter <thederpykrafter@gmail.com>
2024-05-18 18:02:45 -0400

    actually fixed fzf

--- fzf.zsh
+++ fzf.zsh
@@ -1,6 +1,6 @@
 
 # style fzf
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
-export FZF_DEFAULT_COMMAND="fdfind --type f"
+export FZF_DEFAULT_COMMAND="fd --type f"
 
 function fzd() { fd --type d -g $* | fzf }
