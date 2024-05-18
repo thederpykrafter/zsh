899f566ab8f580b819a433457f5cdd2beebb9aad
thederpykrafter <thederpykrafter@gmail.com>
2024-05-17 23:20:43 -0700

    refactor yet again

--- navigation.zsh
+++ navigation.zsh
@@ -6,3 +6,9 @@ alias cd....='cd ../../..'
 
 # mkdir & cd
 function mkcd() { mkdir $1 && cd $1 }
+
+# lsd
+alias ls='lsd'
+
+# vim like exit
+alias :q='exit'
--- shell.zsh
+++ shell.zsh
@@ -4,11 +4,3 @@ alias cls='clear'
 
 # reload zsh
 alias reload='source ~/.zshrc'
-
-# vim like exit
-alias :q='exit'
-
-# lsd
-alias ls='lsd'
-
-
