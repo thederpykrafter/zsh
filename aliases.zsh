07a3c23609258159a82e03d2a36d295f433c3787
thederpykrafter <thederpykrafter@gmail.com>
2024-05-17 23:10:30 -0700

    refactor

--- aliases.zsh
+++ aliases.zsh
@@ -1,17 +1,12 @@
 
+# cls
+alias cls='clear'
+
 # reload zsh
 alias reload='source ~/.zshrc'
 
 # vim like exit
 alias :q='exit'
-
-# cd
-alias cd..='cd ..'
-alias cd...='cd ../..'
-alias cd....='cd ../../..'
-
-# mkdir & cd
-function mkcd() { mkdir $1 && cd $1 }
 
 # nvim alias
 alias vim='nvim'
---
+++ navigation.zsh
@@ -0,0 +1,8 @@
+
+# cd
+alias cd..='cd ..'
+alias cd...='cd ../..'
+alias cd....='cd ../../..'
+
+# mkdir & cd
+function mkcd() { mkdir $1 && cd $1 }
