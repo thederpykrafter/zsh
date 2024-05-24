54f5a19395655c4223bbbabe6cc769f747e783da
thederpykrafter <thederpykrafter@gmail.com>
2024-05-24 13:14:08 -0400

    global npm

--- termux.zsh
+++ termux.zsh
@@ -4,4 +4,5 @@ if [[ -f /data/data/com.termux/files/home/.termux/termux.properties ]]; then
   alias pushconfig="source /data/data/com.termux/files/home/tdk.termux/Scripts/push-config.sh"
   # pull config
   alias pullconfig="source /data/data/com.termux/files/home/tdk.termux/Scripts/pull-config.sh"
+  export PATH=~/.npm_global/bin:$PATH
 fi
