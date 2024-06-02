6e5724b63b0c4e651cc70f8f28ab895e2692e2ed
thederpykrafter <thederpykrafter@gmail.com>
2024-06-02 17:11:24 -0400

    paths

--- paths.zsh
+++ paths.zsh
@@ -1,8 +1,14 @@
 
 if [[ -f /data/data/com.termux/files/home/.termux/termux.properties ]]; then
+    # termux dev path
     export dev="/data/data/com.termux/files/home/storage/shared/Documents/Dev/"
+
+    # termux go path
     export PATH=$PATH:/data/data/com.termux/files/home/go/bin
 else
-    # windows paths
+    # go path
     export PATH=$PATH:/usr/local/go/bin
+
+    # add app images to path
+    export PATH=$PATH:~/Applications
 fi
