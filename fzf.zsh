aca56a9a3876a65f218336ae3de34f806d8a28b8
thederpykrafter <thederpykrafter@gmail.com>
2024-07-15 13:32:08 -0400

    fix fzf

--- fzf.zsh
+++ fzf.zsh
@@ -1,11 +1,5 @@
 
 # global settings
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
-
-if [[ -f /data/data/com.termux/files/home/.termux/termux.properties ]]; then
-    export FZF_DEFAULT_COMMAND="fd --type f"
-    function fzd() { fd --type d -g $* | fzf }
-else
-    export FZF_DEFAULT_COMMAND="fdfind --type f"
-    function fzd() { fdfind --type d -g $* | fzf}
-fi
+export FZF_DEFAULT_COMMAND="fd --type f"
+function fzd() { fd --type d -g $* | fzf}
