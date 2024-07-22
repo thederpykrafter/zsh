b20ace036557072af08e60e85d9688fe07af6da7
thederpykrafter <thederpykrafter@gmail.com>
2024-07-22 08:08:39 -0400

    fixes

--- fzf.zsh
+++ fzf.zsh
@@ -3,10 +3,10 @@ export FZF_DEFAULT_OPTS='--height 50% --layout=reverse --border'
 export FZF_DEFAULT_COMMAND="fd --type f"
 
 # find dir
-function fzd() { fd --type d -g $1 | fzf }
+function fzd() { fd . $1 --type d | fzf }
 
 # find dir and cd
 function fzcd() { cd $(fzd $1) }
 
 # find projects
-function proj() { cd $(fd . ~/Dev/ --type d | fzf) }
+function proj() { fzcd ~/Dev/ }
