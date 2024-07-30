ddd2c37eac563eaeaaded8af0fb8607d8715d137
thederpykrafter <thederpykrafter@gmail.com>
2024-07-30 17:03:51 -0400

    allow modifiers

--- fzf.zsh
+++ fzf.zsh
@@ -3,10 +3,10 @@ export FZF_DEFAULT_OPTS='--height 50% --layout=reverse --border'
 export FZF_DEFAULT_COMMAND="fd --type f"
 
 # find dir
-function fzd() { fd . $1 --type d | fzf }
+function fzd() { fd . $* --type d | fzf }
 
 # find dir and cd
-function fzcd() { cd $(fzd $1) }
+function fzcd() { cd $(fzd $*) }
 
 # find projects
 function proj() {
