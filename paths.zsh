1a5343d01057e6e65c096c80b68e02a43e63559e
thederpykrafter <thederpykrafter@gmail.com>
2024-08-05 23:04:32 -0400

    fix nvm

--- paths.zsh
+++ paths.zsh
@@ -14,4 +14,7 @@ else
 
     # nvm
     [ -z "$NVM_DIR" ] && export NVM_DIR="$HOME/.nvm"
+    source /usr/share/nvm/nvm.sh
+    source /usr/share/nvm/bash_completion
+    source /usr/share/nvm/install-nvm-exec
 fi
