fdf071528c0c2193f7a8145f9e4e96ed23541df3
thederpykrafter <thederpykrafter@gmail.com>
2024-08-09 17:53:34 -0400

    ghinit

--- git.zsh
+++ git.zsh
@@ -4,8 +4,24 @@ alias gg="lazygit"
 # git top level
 alias cg='cd `git rev-parse --show-toplevel`'
 
-# git init, commit, push
+# git init, add, commit
 alias gitinit='git init && git add -A && git commit -m "init"'
+
+# git init, add, commit && gh repo create
+function ghinit() {
+  repo_name=$(echo $PWD | sed "s/\/home\/thederpykrafter\///" | rev | cut -d / -f1 | rev)
+
+  if [ ! -f README.md ]; then
+	  touch README.md
+	  echo "# $repo_name" &> README.md
+  fi
+
+  if [ ! -d .git ]; then
+	  git init && git add -A && git commit -m "init"
+  fi
+
+  gh repo create $repo_name -s . --push --public #2> /dev/null || echo "Remote for \"$repo_name\" already exists"
+}
 
 # .gitignore
 alias gitignore='nvim .gitignore'
