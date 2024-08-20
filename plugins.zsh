0bf0f1cc8c99c9e683730c27161fe91d75c8ae71
thederpykrafter <thederpykrafter@gmail.com>
2024-08-20 02:57:38 -0400

    oops that was wonky

--- plugins.zsh
+++ plugins.zsh
@@ -1,9 +1,10 @@
-if [ -d $ZSH_CUSTOM/plugins/zsh-syntax-highilghting ]; then
-  git clone http://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/
+
+if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/ ]; then
+  git clone http://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
   source ~/.zshrc
 fi
 
-if [ -d $ZSH_CUSTOM/plugins/zsh-autosuggestions ]; then
-  git clone http://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/
+if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+  git clone http://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
   source ~/.zshrc
 fi
