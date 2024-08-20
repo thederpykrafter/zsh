c0036ca67428c423aa9f901c9867ad60ea7c9806
thederpykrafter <thederpykrafter@gmail.com>
2024-08-20 04:04:19 -0400

    keep screen clean

--- plugins.zsh
+++ plugins.zsh
@@ -6,11 +6,13 @@ fi
 
 if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
   git clone http://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+  clear
   source ~/.zshrc
 fi
 
 if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autopair ]; then
   git clone https://github.com/hlissner/zsh-autopair $ZSH_CUSTOM/plugins/zsh-autopair
+  clear
   source ~/.zshrc
 fi
 
