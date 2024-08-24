352d4dc5302c22001a7e451f465b31f420a90dc3
thederpykrafter <thederpykrafter@gmail.com>
2024-08-23 21:17:11 -0400

    remove epmty files from last change lol

--- opam.zsh
+++
@@ -1,6 +0,0 @@
-
-# ocaml configuration
-if [[ -f /data/data/com.termux/files/home/.termux/termux.properties ]]; then
-    return
-else
-fi
--- termux.zsh
+++
@@ -1,7 +0,0 @@
-
-if [[ -f /data/data/com.termux/files/home/.termux/termux.properties ]]; then
-  #push config
-  alias pushconfig="source /data/data/com.termux/files/home/tdk.termux/Scripts/push-config.sh"
-  # pull config
-  alias pullconfig="source /data/data/com.termux/files/home/tdk.termux/Scripts/pull-config.sh"
-fi
