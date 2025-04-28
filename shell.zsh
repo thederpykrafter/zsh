495132eb933819889c5b96643c9c1ffceef445cf
thederpykrafter <thederpykrafter@gmail.com>
2025-04-28 14:28:38 -0400

    sort-downloads alias

--- shell.zsh
+++ shell.zsh
@@ -91,6 +91,9 @@ alias mc-shots="~/Dev/sh/prism-mc-screenshots/mc-shots"
 # not needed if nvidia setup correctly
 #alias godot='godot --rendering-driver opengl3 &'
 
+alias sort-downloads="~/Dev/python/Organize-by-extensions/main.py"
+sort-downloads
+
 function welcome_art() {
   random_image=`shuf -n1 -e \`fd . ~/.oh-my-zsh/custom/images/\``
   if grep "#\!/bin/" $random_image &> /dev/null; then
