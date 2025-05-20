da55ae83089f5c417150bda49935f9dac1aaaf74
thederpykrafter <thederpykrafter@gmail.com>
2025-05-20 19:53:45 -0400

    kvi kickstart alias

--- shell.zsh
+++ shell.zsh
@@ -57,6 +57,7 @@ alias sched='~/Dev/sh/work-sched/work-sched &'
 alias vim='nvim'
 alias vi='nvim'
 alias tvi='NVIM_APPNAME=termux-nvim nvim'
+alias kvi='NVIM_APPNAME=kickstart.nvim nvim'
 # nvim default editor
 export EDITOR='nvim'
 # nvim MANPAGER
