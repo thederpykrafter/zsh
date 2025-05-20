8fc8754ddc9d90267375dbdc49bd35a4bd8a1258
thederpykrafter <thederpykrafter@gmail.com>
2025-05-20 17:20:40 -0400

    make nvim default editor

--- shell.zsh
+++ shell.zsh
@@ -55,6 +55,8 @@ alias sched='~/Dev/sh/work-sched/work-sched &'
 alias vim='nvim'
 alias vi='nvim'
 alias tvi='NVIM_APPNAME=termux-nvim nvim'
+# nvim default editor
+export EDITOR='nvim'
 # nvim MANPAGER
 export MANPAGER='nvim +Man!'
 
