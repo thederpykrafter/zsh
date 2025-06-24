2b78ece8d365ac9cef074b9971005da77afe79c3
thederpykrafter <thederpykrafter@gmail.com>
2025-06-24 08:47:40 -0400

    formatting

--- images/cowsay-fortune.sh
+++ images/cowsay-fortune.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
-cows=`which cowsay | sed 's/bin/share/'`/cows
+cows=$(which cowsay | sed 's/bin/share/')/cows
 
 fortune | cowthink -f $(find $cows -type f | shuf -n 1) | lolcat
